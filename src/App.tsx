ea7fd51e2dc2d34c68ade65cb2eedaa4d6febe95
D. Galagan <dima@airtrail.co>
2024-12-01 14:28:16 +0100

    added missing content

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import {Home} from "./pages/Home";
+import {Favourites} from "./pages/Favourites";
+
+function App() {
+  return (
+   <Routes>
+     <Route path='/' element={<Home />}/>
+     <Route path='/favourites' element={<Favourites />}/>
+   </Routes>
+  );
+}
+
+export default App;
